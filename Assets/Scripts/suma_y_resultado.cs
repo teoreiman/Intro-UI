e9a94411f2d337bfd7ca5f16d361d8e0572c3b6a
teoreiman <teo@reiman.com.ar>
2025-05-13 09:04:50 -0300

    .

---
+++ Assets/Scripts/suma_y_resultado.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class suma_y_resultado : MonoBehaviour
+{
+    public TMP_Inputfield Inputnum1;
+    public TMP_Inputfield Inputnum2;
+    public TextMeshProUGUI txtResultado;
+    int num1;
+    int num2;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       txtResultado.text = ""; 
+    }
+
+        public void suma_y_resultado() {
+        
+        num1 = int.Parse(Inputnum1.text);
+        num2 = int.Parse(Inputnum2.text);
+        txtResultado.text = (num1 + num2).ToString();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+}
